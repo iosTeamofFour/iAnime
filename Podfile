049ac608b9facad9ebcacc799422dedba5ba6e46
Nemesiss.Lin <353388639@qq.com>
2019-10-21 19:02:25 +0800

    Commit for Podfile

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'iAnime' do
+  pod 'AFNetworking', '~> 3.0'
+  pod 'SwiftyJSON', '~> 4.0'
+end
---
+++ Pods/Target Support Files/Pods-iAnime/Pods-iAnime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iAnime : NSObject
+@end
+@implementation PodsDummy_Pods_iAnime
+@end
