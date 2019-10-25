f7927e777a21ae5ddff8740a3aeccf7c370d48c2
Nemesiss.Lin <353388639@qq.com>
2019-10-25 12:00:49 +0800

    完成线段平滑, 接下来准备做根据力度变化线宽

---
+++ iAnime/Views/Drafting/SmoothedBIView.h
@@ -0,0 +1,17 @@
+//
+//  SmoothedBIView.h
+//  iAnime
+//
+//  Created by NemesissLin on 2019/10/25.
+//  Copyright © 2019 NemesissLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SmoothedBIView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iAnime/Views/Drafting/SmoothedBIView.m
@@ -0,0 +1,21 @@
+//
+//  SmoothedBIView.m
+//  iAnime
+//
+//  Created by NemesissLin on 2019/10/25.
+//  Copyright © 2019 NemesissLin. All rights reserved.
+//
+
+#import "SmoothedBIView.h"
+
+@implementation SmoothedBIView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
